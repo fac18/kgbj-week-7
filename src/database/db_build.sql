4cf64a2e3c66f11b2e1e700bfa6db505db4d5c46
Bethany Scott <bethanyioscott@hotmail.co.uk>
2019-12-04 16:52:40 +0000

    Set up js and sql files for database

--- src/database/db_build.sql
+++ src/database/db_build.sql
@@ -3,12 +3,6 @@ BEGIN;
 DROP TABLE IF EXISTS users CASCADE;
 DROP TABLE IF EXISTS house CASCADE;
 
-CREATE TABLE users (
-  id SERIAL PRIMARY KEY,
-  name VARCHAR(100) NOT NULL,
-  house_id SERIAL FOREIGN KEY,
-  points INTEGER DEFAULT 0
-);
 
 CREATE TABLE house (
   id SERIAL PRIMARY KEY,
@@ -16,8 +10,18 @@ CREATE TABLE house (
   attributes VARCHAR NOT NULL,
   total_house_points INTEGER DEFAULT 0
 );
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(100) NOT NULL,
+  house_id integer REFERENCES house(id) ON UPDATE CASCADE,
+  points INTEGER DEFAULT 0
+);
 
-INSERT INTO  (name, ) VALUES
-();
+
+INSERT INTO house (house, attributes) VALUES
+('Gryffindor', 'Brave and bold'),
+('Hufflepuff', 'Kind and gentle'),
+('Ravenclaw', 'Honest and practical'),
+('Slytherin', 'Clever and competitive');
 
 COMMIT;
