d8cc7a3eafd53529afcd3c7b397326564c2256f3
Bethany Scott <bethanyioscott@hotmail.co.uk>
2019-12-04 16:59:10 +0000

    Insert house_id into users

--- src/database/db_build.sql
+++ src/database/db_build.sql
@@ -4,6 +4,7 @@ DROP TABLE IF EXISTS users CASCADE;
 DROP TABLE IF EXISTS house CASCADE;
 
 
+
 CREATE TABLE house (
   id SERIAL PRIMARY KEY,
   house VARCHAR(100) NOT NULL,
@@ -17,11 +18,16 @@ CREATE TABLE users (
   points INTEGER DEFAULT 0
 );
 
-
 INSERT INTO house (house, attributes) VALUES
 ('Gryffindor', 'Brave and bold'),
 ('Hufflepuff', 'Kind and gentle'),
 ('Ravenclaw', 'Honest and practical'),
 ('Slytherin', 'Clever and competitive');
+
+INSERT INTO users (name, house_id) VALUES
+('Jamie', 1),
+('Alex', 2),
+('Rosa', 3),
+('Beth', 4);
 
 COMMIT;
