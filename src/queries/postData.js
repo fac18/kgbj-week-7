260847b216b8d574d75f74ece1b899e0c81fc301
Rosalie-Baxter <rosalie.baxter@outlook.com>
2019-12-05 18:00:41 +0000

    added points functionality

--- src/queries/postData.js
+++ src/queries/postData.js
@@ -1,9 +1,9 @@
 const dbConnection = require("../database/db_connection.js");
 
-const postData = (name, house, cb) => {
+const postData = (name, house, points, cb) => {
   dbConnection.query(
-    "INSERT INTO users (name, house_name) VALUES ($1, $2)",
-    [name, house],
+    "INSERT INTO users (name, house_name, points) VALUES ($1, $2, $3)",
+    [name, house, points],
     (err, res) => {
       if (err) return cb(err);
       cb(null, res);
