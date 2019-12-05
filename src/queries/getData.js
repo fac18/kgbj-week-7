d1689c3eede1a61d3b861882234b614097c07efa
Alexandra O'Mahony <alexandra.omahony1@gmail.com>
2019-12-05 11:19:11 +0000

    added getData functionality finding issues with get and post simultaneously

--- src/queries/getData.js
+++ src/queries/getData.js
@@ -1,5 +1,10 @@
-const getData = () => {
-    console.log("getDatapoop")
+const dbConnection = require("../database/db_connection.js");
+
+const getData = cb => {
+  dbConnection.query("SELECT * FROM users", (err, res) => {
+    if (err) return cb(err);
+    cb(null, res.rows);
+  });
 };
 
 module.exports = getData;
