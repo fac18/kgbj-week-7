f72b71be358155a63444d89b45d4e20c07f3b2d8
Bethany Scott <bethanyioscott@hotmail.co.uk>
2019-12-05 15:03:13 +0000

    Create test database and connected files

---
+++ tests/tests.js
@@ -0,0 +1,20 @@
+const tape = require("tape");
+const runDbBuild = require("../src/database/db_build");
+const getData = require("../src/queries/getData");
+const postData = require("../src/queries/postData");
+
+tape("tape is working", t => {
+  t.equals(1, 1, "one equals one");
+  t.end();
+});
+
+tape('test getData', t => {
+  runDbBuild(function(err,res){
+    let actual =  '';
+  console.log(actual);
+    let expected = getData();
+    console.log(expected);
+    t.equals(actual, expected, 'getData');
+    t.end();
+  })
+})
