15e154fb68c245ffa6bcf51ef985011a3b06a036
Rosalie-Baxter <rosalie.baxter@outlook.com>
2019-12-06 11:38:24 +0000

    sorted by house

--- src/queries/getData.js
+++ src/queries/getData.js
@@ -1,7 +1,7 @@
 const dbConnection = require("../database/db_connection.js");
 
 const getData = cb => {
-  dbConnection.query("SELECT * FROM users", (err, res) => {
+  dbConnection.query("SELECT * FROM users ORDER BY house_name", (err, res) => {
     if (err) {return cb(err)}
     else {
     cb(null, res.rows)
